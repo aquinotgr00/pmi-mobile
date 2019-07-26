3cb208a2f2d1b440763a9b39e2fc966e05f0574b
Aquino Tegar <aquinotgr@pm.me>
2019-07-27 02:10:38 +0700

    add element to home scree

--- App.js
+++ App.js
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import {
-//   createStackNavigator,
-//   createAppContainer
-// } from 'react-navigation'
-
-// // Components
-// import Home from './components/Home'
-// import Campaign from './components/Campaign'
-// import CampaignList from './components/CampaignList'
-
-// const RootStack = createStackNavigator({
-//   Home: { screen: Home },
-//   Campaign: { screen: Campaign },
-//   CampaignList: { screen: CampaignList },
-// },
-// {
-//   initialRouteName: 'Home',
-//   defaultNavigationOptions: {
-//     headerTintColor: '#fff',
-//     headerBackTitle: null
-//   }
-// })
-
-// const App = createAppContainer(RootStack)
-
-// export default App
 import React, { Component } from 'react'
 
 import { Provider } from 'react-redux'
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,45 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { IconInu, Screen } from 'src/components'
 
 export default class HomeScreen extends React.Component {
+    state = {
+      loading: true,
+      generalCampaigns: []
+    }
+
     static navigationOptions = {
       drawerLabel: 'Home',
       drawerIcon: ({ tintColor }) => (
         <IconInu name='icon-pmi-home' color={tintColor} />
       ),
     };
+
+    async componentDidMount() {
+      // const generalCampaigns = await basicRequest().get('/app/campaigns?t=1')
+      // this.setState({
+      //   generalCampaigns
+      // })
+    }
   
     render() {
       return (
         <Screen title='Home' menu>
-          <Text>ini Home Screen</Text>
+          <View style={{flex:1,flexDirection:'row',marginBottom:15}}>
+            <Text style={[styles.campaignTitleList, {width: '50%'}]}>Donasi Umum</Text>
+            <TouchableOpacity style={{width: '50%'}} onPress={() => navigation.navigate('CampaignList', {param: 'donasi-umum'})}>
+              <Text style={{textAlign: 'right',color: 'red',fontSize: 13,fontWeight: '600'}}>Lihat Semua</Text>
+            </TouchableOpacity>
+          </View>
         </Screen>
       );
     }
-  }
+}
+
+const styles = StyleSheet.create({
+  campaignTitleList: {
+    fontWeight: '500',
+    fontSize: 16,
+    letterSpacing: .5,
+  },
+})
