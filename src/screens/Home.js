4198a9901fb6024a50df2cbc93c0f4e71e1d5526
Paulus Otto Harman <lonewolfffffff@gmail.com>
2019-07-27 16:32:17 +0700

    midtrans integration

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Button } from 'native-base'
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { IconInu, Screen } from 'src/components'
+import Config from 'react-native-config'
+import PaymentGateway from 'react-native-payment-gateway'
 
 export default class HomeScreen extends React.Component {
   state = {
@@ -20,6 +22,7 @@ export default class HomeScreen extends React.Component {
     super(props)
   
     this.navigateToInKindDonationForm = this.navigateToInKindDonationForm.bind(this)
+    this.pay = this.pay.bind(this)
   }
   
   navigateToInKindDonationForm() {
@@ -33,11 +36,77 @@ export default class HomeScreen extends React.Component {
     // })
   }
 
+  pay() {
+    const optionConect = {
+      clientKey: 'SB-Mid-client-5cSArh5V34nHg_JD',
+      urlMerchant: 'https://webapi-develop-pmi-public.blm.solutions',
+      sandbox : true
+    }
+    console.log(optionConect);
+    
+    const transRequest = {
+        transactionId: "0001",
+        totalAmount: 4000
+    }
+
+    const itemDetails = [
+        {id: "001", price: 1000, qty: 4, name: "peanuts"}
+    ];
+
+    const creditCardOptions = {
+        saveCard: false,
+        saveToken: false,
+        paymentMode: "Normal",
+        secure: false
+    };
+
+    const userDetail = {
+        fullName: "jhon",
+        email: "jhon@payment.com",
+        phoneNumber: "0850000000",
+        userId: "U01",
+        address: "street coffee",
+        city: "yogyakarta",
+        country: "IDN",
+        zipCode: "59382"
+    };
+
+    const optionColorTheme = {
+        primary: '#c51f1f',
+        primaryDark: '#1a4794',
+        secondary: '#1fce38'
+    }
+
+    const optionFont = {
+        defaultText: "open_sans_regular.ttf",
+        semiBoldText: "open_sans_semibold.ttf",
+        boldText: "open_sans_bold.ttf"
+    }
+
+    const callback = (res) => {
+        console.log(res)
+    };
+
+    PaymentGateway.checkOut(
+        optionConect,
+        transRequest,
+        itemDetails,
+        creditCardOptions,
+        userDetail,
+        optionColorTheme,
+        optionFont,
+        callback
+    );
+  }
+
   render() {
     return (
       <Screen title='Home' menu>
         <Button bordered onPress={this.navigateToInKindDonationForm}>
           <Text>Mock Donasi Barang</Text>
+        </Button>
+        <Button bordered onPress={this.pay}>
+          <Text>Mock Midtrans</Text>
         </Button>
         <View style={{flex:1,flexDirection:'row',marginBottom:15}}>
           <Text style={[styles.campaignTitleList, {width: '50%'}]}>Donasi Umum</Text>
