c0ee0607ceec81d3d1ec933fb2abf5500c944354
Paulus Otto Harman <lonewolfffffff@gmail.com>
2019-07-30 07:58:02 +0700

    In Kind Donation form : handle add item

--- src/components/CampaignList/index.js
+++ src/components/CampaignList/index.js
@@ -5,57 +5,63 @@ import { CampaignCard } from './CampaignCard'
 export { CampaignCard } from './CampaignCard'
 
 export default class CampaignList extends Component {
-	constructor (props) {
-		super(props)
+  constructor (props) {
+    super(props)
 
-		this.navigateToDetailCampaign = this.navigateToDetailCampaign.bind(this)
-		this.navigateToCampaignList = this.navigateToCampaignList.bind(this)
-		this.navigateToInputDonation = this.navigateToInputDonation.bind(this)
-	}
+    this.navigateToDetailCampaign = this.navigateToDetailCampaign.bind(this)
+    this.navigateToCampaignList = this.navigateToCampaignList.bind(this)
+    this.navigateToInputDonation = this.navigateToInputDonation.bind(this)
+  }
 
-	navigateToDetailCampaign(id) {
-		this.props.navigation.navigate('Campaign', {id})
-	}
-	navigateToCampaignList() {
-		const { title, type } = this.props
-		this.props.navigation.navigate('CampaignList', {title, type})
-	}
-	navigateToInputDonation(id) {
-		this.props.navigation.navigate('FundDonation', {id})
-	}
+  navigateToDetailCampaign (id) {
+    this.props.navigation.navigate('Campaign', { id })
+  }
 
-	render () {
-		return (
-			<>
-				<View style={{
-					flex:1,
-					flexDirection: 'row',
-					marginBottom: 15,
-				}}>
-					<Text style={{width:'50%',fontWeight:'500',fontSize:17}}>{this.props.title}</Text>
-					<TouchableOpacity
-						onPress={this.navigateToCampaignList}
-						style={{width:'50%',justifyContent:'center'}}
-					>
-						<Text style={{textAlign:'right',color:'red',fontSize:12,fontWeight:'500'}}>Lihat Semua</Text>
-					</TouchableOpacity>
-				</View>
+  navigateToCampaignList () {
+    const { title, type } = this.props
+    this.props.navigation.navigate('CampaignList', { title, type })
+  }
 
-				<FlatList
-					data={this.props.data}
-					style={{marginBottom:25,paddingBottom:10}}
-					horizontal
-					keyExtractor={(item, index) => item.id}
-					renderItem={(item) =>
-						<CampaignCard
-							{...item}
-							width={245}
-							link={this.navigateToDetailCampaign}
-							linkForm={this.navigateToInputDonation}
-						/>
-					}
-				/>
-			</>
-		)
-	}
+  navigateToInputDonation (id, fundraising) {
+    let routeName = 'InKindDonationForm'
+    if (fundraising) {
+      routeName = 'FundDonation'
+    }
+    this.props.navigation.navigate(routeName, { id })
+  }
+
+  render () {
+    return (
+      <>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginBottom: 15
+        }}>
+          <Text style={{ width: '50%', fontWeight: '500', fontSize: 17 }}>{this.props.title}</Text>
+          <TouchableOpacity
+            onPress={this.navigateToCampaignList}
+            style={{ width: '50%', justifyContent: 'center' }}
+          >
+            <Text style={{ textAlign: 'right', color: 'red', fontSize: 12, fontWeight: '500' }}>Lihat Semua</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={this.props.data}
+          style={{ marginBottom: 25, paddingBottom: 10 }}
+          horizontal
+          keyExtractor={(item, index) => `${item.id}`}
+          renderItem={(item) =>
+            <CampaignCard
+              {...item}
+              width={245}
+              link={this.navigateToDetailCampaign}
+              linkForm={this.navigateToInputDonation}
+            />
+          }
+        />
+      </>
+    )
+  }
 }
--- src/screens/Donation/InKind.js
+++ src/screens/Donation/InKind.js
@@ -1,51 +1,109 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { View } from 'react-native'
 import { Button, CheckBox, Icon, Input, Item, Picker, Text } from 'native-base'
-import { Screen } from 'src/components'
+import { RedButton, Screen } from 'src/components'
+import Color from 'src/constants/Color'
 
-export default class InKindDonationFormScreen extends React.Component {
+class InKindDonationFormScreen extends React.Component {
   constructor (props) {
     super(props)
-
+    this.state = {
+      items: []
+    }
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.addItem = this.addItem.bind(this)
+    this.onValueChange = this.onValueChange.bind(this)
   }
 
   handleFormSubmit () {
-    console.log('submitted!')
+    const { token } = this.props.user
+    let routeName = 'DonatorNavigator'
+    if (token) {
+      routeName = 'GuestNavigator'
+    }
+    this.props.navigation.navigate(routeName)
+  }
+
+  addItem () {
+    this.setState({ items: [...this.state.items, { itemType: '', name: '', qty: 0 }] })
+  }
+
+  onValueChange () {
+
   }
 
   render () {
     return (
       <Screen title='Berdonasi Barang' back>
-        <Text>Informasi Donasi</Text>
-        <Item>
-          <Input placeholder='Nama' />
-        </Item>
-        <Item>
-          <Input placeholder='Email' />
-        </Item>
-        <Item>
-          <Input placeholder='Nomor HP' />
-        </Item>
+        <View style={{ paddingBottom: 30 }}>
+          <Text style={{ fontWeight: '600', fontSize: 16, marginVertical: 8 }}>Informasi Donasi</Text>
+          <Item>
+            <Input placeholder='Nama' />
+          </Item>
+          <Item>
+            <Input placeholder='Email' />
+          </Item>
+          <Item>
+            <Input placeholder='Nomor HP' />
+          </Item>
 
-        <Picker
-          mode='dropdown'
-          iosIcon={<Icon name='arrow-down' />}
-          placeholder='Metode Penyerahan'
-          placeholderStyle={{ color: '#bfc6ea' }}
-          placeholderIconColor='#007aff'
-          style={{ width: undefined }}
-        >
-          <Picker.Item label='Barang dikirim' value='key0' />
-          <Picker.Item label='Barang diantar' value='key1' />
-        </Picker>
+          <Picker
+            mode='dropdown'
+            iosIcon={<Icon name='arrow-down' />}
+            placeholder='Metode Penyerahan'
+            placeholderStyle={{ color: '#bfc6ea' }}
+            placeholderIconColor='#007aff'
+            style={{ width: undefined }}
+            onValueChange={this.onValueChange}
+          >
+            <Picker.Item label='Barang dikirim' value='key0' />
+            <Picker.Item label='Barang diantar' value='key1' />
+          </Picker>
 
-        <Text>Barang Donasi</Text>
-        
-        <CheckBox checked /><Text>Sembunyikan nama saya (Anonim)</Text>
-        <Button rounded danger onPress={this.handleFormSubmit}>
-          <Text>Lanjutkan</Text>
-        </Button>
+          <Text style={{ fontWeight: '600', fontSize: 16, marginVertical: 8 }}>Barang Donasi</Text>
+
+          {
+            this.state.items.map((item, i) => (
+              <DonationItem
+                itemId={i}
+                key={`${i}`}
+              />
+            ))
+          }
+
+          <Button transparent full onPress={this.addItem}>
+            <Text style={{ color: Color.red }}>Tambah Barang +</Text>
+          </Button>
+
+          <View style={{ flexDirection: 'row', marginVertical: 20 }}>
+            <CheckBox color='red' style={{ borderRadius: 3 }} />
+            <Text style={{ marginLeft: 20 }}>Sembunyikan nama saya (Anonim)</Text>
+          </View>
+          { this.state.items.length > 0 && <RedButton text='Lanjutkan' onPress={this.handleFormSubmit} style={{ marginTop: 30 }} /> }
+        </View>
       </Screen>
+    )
+  }
+}
+
+export default connect(state => ({ user: state.user }))(InKindDonationFormScreen)
+
+class DonationItem extends React.Component {
+  render () {
+    console.log(this.props.itemId)
+    return (
+      <View style={{ borderRadius: 10, borderWidth: 1, borderColor: Color.lightGray, padding: 5, marginVertical: 10 }}>
+        <Item>
+          <Input placeholder='Jenis Barang Donasi' />
+        </Item>
+        <Item>
+          <Input placeholder='Nama Barang' />
+        </Item>
+        <Item>
+          <Input placeholder='Jumlah Barang' />
+        </Item>
+      </View>
     )
   }
 }
