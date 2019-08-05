48ae10d2b6e33da49e41685b45014662c9374c78
Paulus Otto Harman <lonewolfffffff@gmail.com>
2019-08-05 22:29:35 +0700

    home : pull to refresh

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import { Image, RefreshControl } from 'react-native'
 import { Screen } from 'src/components'
 import SearchBar from './SearchBar'
 import DonationTypeButtons from './DonationTypeButtons'
 import RecentCampaigns from './RecentCampaigns'
 import HomeBanner from './HomeBanner'
-import { Image } from 'react-native'
 
 export default class HomeScreen extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      donationType: undefined
+      refreshing: false,
+      donationType: undefined,
+      timestamp: Date.now()
     }
 
     this.handleChangeDonationType = this.handleChangeDonationType.bind(this)
+    this.onRefresh = this.onRefresh.bind(this)
+  }
+
+  onRefresh () {
+    this.setState({ timestamp: Date.now() })
   }
 
   handleChangeDonationType (donationType) {
@@ -27,7 +34,17 @@ export default class HomeScreen extends React.Component {
 
   render () {
     return (
-      <Screen unpadded title={<Image source={require('assets/images/logo-home.png')} />} menu>
+      <Screen
+        menu
+        unpadded
+        title={<Image source={require('assets/images/logo-home.png')} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this.onRefresh}
+          />
+        }
+      >
         <SearchBar />
         <DonationTypeButtons
           activeButton={this.state.donationType}
@@ -38,18 +55,21 @@ export default class HomeScreen extends React.Component {
           numberOfCampaigns={3}
           campaignType={1}
           donationType={this.state.donationType}
+          lastUpdate={this.state.timestamp}
         />
         <RecentCampaigns
           title='Donasi Khusus'
           numberOfCampaigns={3}
           campaignType={2}
           donationType={this.state.donationType}
+          lastUpdate={this.state.timestamp}
         />
         <HomeBanner navigation={this.props.navigation} />
         <RecentCampaigns
           title='Bulan Dana'
           numberOfCampaigns={3}
           campaignType={3}
+          lastUpdate={this.state.timestamp}
         />
       </Screen>
     )
